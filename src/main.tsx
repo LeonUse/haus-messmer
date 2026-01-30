a189301a40d1db8b3d2e79255430f712e89c5b89
Leon Braun <leonbraunspam@gmail.com>
2026-01-30 21:51:16 +0100

    Fix routing: Add basename to BrowserRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/haus-messmer">
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
